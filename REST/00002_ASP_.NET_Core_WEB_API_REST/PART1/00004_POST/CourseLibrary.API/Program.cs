ca4ed90eca349bffbef3c0e301037b0f0f5bbebc
BANDA Ranjit <ranjit.banda@hexagon.com>
2022-12-23 13:14:07 +0530

    POST is completed in REST including validations

--- REST/00002_ASP_.NET_Core_WEB_API_REST/PART1/00004_POST/CourseLibrary.API/Program.cs
+++ REST/00002_ASP_.NET_Core_WEB_API_REST/PART1/00004_POST/CourseLibrary.API/Program.cs
@@ -1,6 +1,8 @@
 using CourseLibrary.API.DataStore;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -18,7 +20,28 @@ builder.Services.AddControllers(setupAction =>
     setupAction.ReturnHttpNotAcceptable = true;
     //setupAction.OutputFormatters.Add(
     //    new XmlDataContractSerializerOutputFormatter());
-}).AddXmlDataContractSerializerFormatters();
+}).AddXmlDataContractSerializerFormatters()
+.ConfigureApiBehaviorOptions(setupAction =>
+{
+    setupAction.InvalidModelStateResponseFactory = context =>
+    {
+        var problemDetails = new ValidationProblemDetails(context.ModelState)
+        {
+            Type = "https://courselibrary.com/modelvalidationproblem",
+            Title = "One or more model validation errors occurred.",
+            Status = StatusCodes.Status422UnprocessableEntity,
+            Detail = "See the errors property for details.",
+            Instance = context.HttpContext.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+        return new UnprocessableEntityObjectResult(problemDetails)
+        {
+            ContentTypes = { "application/problem+json" }
+        };
+    };
+});
 
 //LETS MAKE SURE THAT THE AUTOMAPPER SERVICES ARE REGISTERED IN THE CONTAINER
 //We are loading all profiles from all the assemblies
